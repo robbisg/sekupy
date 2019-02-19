a04e2431585ae2cb4d61997f4d8f16e775d6fcdd
robbisg <robbenson18@gmail.com>
2019-02-19 11:24:20 +0100

    NF: Added fa attributes on node names

--- pyitab/io/connectivity.py
+++ pyitab/io/connectivity.py
@@ -13,16 +13,23 @@ from pyitab.io.configuration import read_configuration
 from pyitab.analysis import Node
 from pyitab.io.base import load_subject_file, add_attributes, load_attributes,\
     add_subjectname, load_filelist
+from pyitab.utils.atlas import get_atlas_info
 
 # TODO : Document 
 
 def conn_transformer(data):
     
     data = np.rollaxis(np.vstack(data), 0, 3)
+    parcels = data.shape[0]
     data = data[np.triu_indices(data.shape[0], k=1)].T
     logger.debug(data.shape)
     
-    return data
+    return data, parcels
+
+
+def power_transformer(data):
+    data = np.vstack(data)
+    return data, data.shape[1]
     
 
 def load_mat_data(path, subj, folder, **kwargs):
@@ -30,7 +37,7 @@ def load_mat_data(path, subj, folder, **kwargs):
     from scipy.io import loadmat
     
     meg_transformer = {'connectivity': conn_transformer,
-                       'power': lambda data: np.vstack(data)}
+                       'power': power_transformer}
     
     key = kwargs['mat_key']
     transformer = meg_transformer[kwargs['transformer']]
@@ -57,7 +64,7 @@ def load_mat_data(path, subj, folder, **kwargs):
 
 def load_mat_ds(path, subj, folder, **kwargs):   
         
-    data = load_mat_data(path, subj, folder, **kwargs)
+    data, parcels = load_mat_data(path, subj, folder, **kwargs)
     
     # load attributes
     attr = load_attributes(path, subj, folder, **kwargs)
@@ -70,7 +77,9 @@ def load_mat_ds(path, subj, folder, **kwargs):
     ds = Dataset.from_wizard(data, attr.targets)
     ds = add_subjectname(ds, subj)
     ds = add_attributes(ds, attr)
-    
+
+    ds = add_labels(ds, parcels, **kwargs)
+
     #ds.fa['roi_labels'] = labels
     ds.fa['matrix_values'] = np.ones_like(data[0])
     
@@ -79,6 +88,23 @@ def load_mat_ds(path, subj, folder, **kwargs):
     return ds
 
 
+def add_labels(ds, parcels, **kwargs):
+
+    labels = get_atlas_info(kwargs['atlas'])[4]
+    labels = labels[:parcels]
+
+    if kwargs['transformer'] == 'connectivity':
+        idx_from, idx_to = np.triu_indices(parcels, k=1)
+        nodes_from = [labels[i] for i in idx_from]
+        nodes_to = [labels[i] for i in idx_to]
+        ds.fa['nodes_1'] = nodes_from
+        ds.fa['nodes_2'] = nodes_to
+    else:
+        ds.fa['nodes'] = labels
+
+    # TODO: Power labels
+
+    return ds
 
 
 def edit_attr(attr, shape):
