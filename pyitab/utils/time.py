e25ae7175806ea6a7ca6153f945f5b5df63c3012
robbisg <robbenson18@gmail.com>
2019-04-24 16:55:26 +0200

    RF: Refactored get_time

--- pyitab/utils/time.py
+++ pyitab/utils/time.py
@@ -1,7 +1,7 @@
 import time
 
 
-def get_time():
+def get_time(elements=6):
     """Get the current time and returns a string (fmt: yymmdd_hhmmss)"""
     
     # Time acquisition
@@ -9,7 +9,7 @@ def get_time():
     
     datetime = ''
     i = 0
-    for elem in tempo[:-3]:
+    for elem in tempo[:elements]:
         i = i + 1
         if len(str(elem)) < 2:
             elem = '0'+str(elem)
