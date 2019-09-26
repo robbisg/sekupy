857cb35decc401f1d0a7176143b8d41cbb832884
robbisg <robbenson18@gmail.com>
2019-09-26 18:25:17 +0200

    NF: Improved memory reducer

--- pyitab/preprocessing/memory.py
+++ pyitab/preprocessing/memory.py
@@ -16,5 +16,39 @@ class MemoryReducer(Transformer):
         logger.info("Converted ds to %s", str(self._dtype))
         
         ds.samples = self._dtype(ds.samples)
+
+        for attribute in ds.fa.keys():
+            array = ds.fa[attribute].value
+            ds.fa[attribute] = self._minify(array)
+
+        for attribute in ds.sa.keys():
+            array = ds.sa[attribute].value
+            ds.sa[attribute] = self._minify(array)
+
                         
         return Transformer.transform(self, ds)
+
+    
+    def _minify(self, array):
+
+        if np.issubdtype(array.dtype, np.str):
+            return array
+        
+        elif np.issubdtype(array.dtype, np.float):
+            return np.float16(array)
+
+        elif np.issubdtype(array.dtype, np.integer):
+            return self._check_int(array)
+
+    
+    def _check_int(self, array):
+        if np.abs(array).max() > 127:
+            dtype = np.int16
+        elif np.abs(array).max() > 32767:
+            dtype = np.int32
+        else:
+            dtype = np.int8
+
+        return dtype(array)
+        
+
