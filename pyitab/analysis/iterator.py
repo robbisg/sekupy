ecb792dc843cb5644307bc6a71040b776b10a23d
Guidotti Roberto <guidotr1@cs-087.org.aalto.fi>
2020-02-26 10:44:40 +0200

    NF+DOC: combined options added #61

--- pyitab/analysis/iterator.py
+++ pyitab/analysis/iterator.py
@@ -1,24 +1,55 @@
 from pyitab.utils import get_id
 import numpy as np
 import logging
+import itertools
+from pyitab.analysis.utils import get_params
 logger = logging.getLogger(__name__)
 
 
 class AnalysisIterator(object):
 
-    # TODO: Document other kind of options
+
     def __init__(self, options, configurator, kind='combination'):
         """This class allows to configure different analysis to be
         iterated using a set of options.
 
-        _default_options = {
-       
-                       'sampleslicer__condition' : [['vipassana'], ['samatha']],
-                       'estimator__svr__C': [1, 10],                          
-                       'cv__n_splits': [10, 15],
-                       'analysis__radius': [9,18],
-                           
-                        }
+        Options can have different formats (see ```kind``` attributes).
+        - ```combination```:
+            _default_options = 
+                    {'sample_slicer__condition' : [['vipassana'], ['samatha']],
+                    'estimator__svr__C': [1, 10],                          
+                    'cv__n_splits': [10, 15],
+                    'analysis__radius': [9,18]}
+
+        Above config runs every combination of each single dictionary entry.
+        
+        - ```configurations```:
+            _default_options = [
+                {'sample_slicer__condition' : ['vipassana'],
+                'estimator__svr__C': 1, 
+                'cv__n_splits': 10,
+                'analysis__radius': 9},
+                {'sample_slicer__condition' : ['samatha'],
+                'estimator__svr__C': 10,
+                'cv__n_splits': 15,
+                'analysis__radius': 18}
+            ]
+        
+        This runs two analyses, with configurations specified in elements of the list.
+
+        - ```list```
+            _default_options = 
+                    {'sample_slicer__condition' : [['vipassana'], ['samatha']],
+                    'estimator__svr__C': [1, 10],                          
+                    'cv__n_splits': [10, 15],
+                    'analysis__radius': [9,18]}
+        
+        In this case, two configurations are built. The first with elements in 
+        position 0 of each list and the other with elements in position 1. 
+        The result is the same as the example with ```configurations```.
+        Elements in the lists must be equal otherwise combination will be performed.
+
+     
                 
         Parameters
         ----------
@@ -47,6 +78,8 @@ class AnalysisIterator(object):
             list_opt = options[:]
             options = dict()
             options['options'] = list_opt
+        elif kind == 'combined':
+            fx = self._combined_setup
         else:
             fx = self._setup
         
@@ -66,7 +99,7 @@ class AnalysisIterator(object):
 
     def _list_setup(self, **kwargs):
         
-        import itertools
+        
         set_ = set([len(value) for key, value in kwargs.items()])
 
         if len(set_) != 1:
@@ -90,9 +123,51 @@ class AnalysisIterator(object):
         return configurations, i, n
 
 
-   
-    def _setup(self, **kwargs):
+    def _combined_setup(self, **kwargs):
+        # This may be replaced by _build_combinations
+        kwargs = self._check_subject_keywords(**kwargs)
+        logger.info(kwargs)
+        return self._build_combinations(key='estimator', **kwargs)
         
+
+    def _build_combinations(self, key='estimator', **kwargs):
+        keyword_list = {k: v for k, v in kwargs.items() if k.find(key) != -1}
+        excluded_params = {k: v for k, v in kwargs.items() if k.find(key) == -1}
+
+        objects = keyword_list.pop(key)
+        
+        params = get_params(keyword_list, key)
+        
+        combination_list = []
+        for obj in objects:
+            name = obj[0][0][0]
+            est_params = get_params(params, name)
+
+            if len(est_params) == 0:
+                combination_list += [{'estimator': obj}]
+                continue
+            
+            param_list = [v for k, v in est_params.items()]
+            
+            list_ = list(itertools.product(obj, *param_list))
+            keys_ = ['estimator']
+            keys_ += ["estimator__%s__%s" % (name, p) for p in est_params.keys()]
+            combination_list += [dict(zip(keys_, elem)) for elem in list_]
+        
+        excluded_list, _, _ = self._setup(**excluded_params)
+
+        configurations = []
+
+        for option_excluded in excluded_list:
+            for option_included in combination_list:
+                keys = list(option_excluded.keys()) + list(option_included.keys())
+                values = list(option_excluded.values()) + list(option_included.values())
+                configurations.append(dict(zip(keys, values)))
+
+        return configurations, 0, len(configurations)
+
+
+    def _check_subject_keywords(self, **kwargs):
         import itertools
         subject_keys = ['sample_slicer__subject', 
                         'fetch__subject_names']
@@ -104,6 +179,15 @@ class AnalysisIterator(object):
                 self.n_subjects = len(v)
             
         logger.debug(self.n_subjects)
+
+        return kwargs
+
+
+   
+    def _setup(self, **kwargs):
+        
+        kwargs = self._check_subject_keywords(**kwargs)
+
         args = [arg for arg in kwargs]
         logger.info(kwargs)
         combinations_ = list(itertools.product(*[kwargs[arg] for arg in kwargs]))
--- pyitab/analysis/tests/test_iterator.py
+++ pyitab/analysis/tests/test_iterator.py
@@ -3,6 +3,8 @@ from pyitab.analysis.pipeline import AnalysisPipeline
 from pyitab.analysis.iterator import AnalysisIterator
 from sklearn.model_selection import StratifiedShuffleSplit, LeaveOneGroupOut
 from pyitab.analysis.decoding.roi_decoding import RoiDecoding
+from pyitab.analysis.states.base import Clustering
+from sklearn import cluster, mixture
 from pyitab.analysis.utils import get_params
 
 
@@ -11,14 +13,161 @@ from pyitab.tests import fetch_ds, get_datadir, tmpdir
 import pytest
 import numpy as np
 
+
 def test_iterator_combination():
-    pass
+    
+    options = {
+            'sample_slicer__subject': [['subj01'], ['subj02']],
+            'estimator__clf__C': [1, 2, 3],
+            'cv__n_splits': [2, 5]
+
+    }
+
+    example_configuration = {   
+                                'prepro': ['sample_slicer', 'target_transformer'],
+                                 
+                                'sample_slicer__decision': ['L', 'F'],
+                                'target_transformer__attr': 'decision',
+                                
+                                'estimator__clf__C': 1,
+                                'estimator__clf__kernel': 'linear',
+                                
+                                'cv': StratifiedShuffleSplit,
+                                'cv__n_splits': 2,
+                                'cv__test_size': 0.2,
+                                                            
+                                'analysis': RoiDecoding,
+                                'cv_attr': 'chunks'
+                            }
+
+    iterator = AnalysisIterator(options, 
+                                AnalysisConfigurator(**example_configuration))
+
+    n_options = np.prod([len(v) for k, v in options.items()])
+
+    assert len(list(iterator)) == n_options
+
+
 
 def test_iterator_list():
-    pass
 
-def test_iterator_dict():
-    pass
+    options = {
+            'sample_slicer__subject': [['subj01'], ['subj02']],
+            'estimator__clf__C': [1, 2],
+            'cv__n_splits': [2, 5]
+
+    }
+
+    example_configuration = {   
+                                'prepro': ['sample_slicer', 'target_transformer'],
+                                 
+                                'sample_slicer__decision': ['L', 'F'],
+                                'target_transformer__attr': 'decision',
+                                
+                                'estimator__clf__C': 1,
+                                'estimator__clf__kernel': 'linear',
+                                
+                                'cv': StratifiedShuffleSplit,
+                                'cv__n_splits': 2,
+                                'cv__test_size': 0.2,
+                                                            
+                                'analysis': RoiDecoding,
+                                'cv_attr': 'chunks'
+                            }
+
+    iterator = AnalysisIterator(options, 
+                                AnalysisConfigurator(**example_configuration),
+                                kind='list'
+                                )
+
+    n_options = len(options['cv__n_splits'])
+    assert len(list(iterator)) == n_options
+
+    # Failing
+    options = {
+            'sample_slicer__subject': [['subj01'], ['subj02']],
+            'estimator__clf__C': [1, 2],
+            'cv__n_splits': [2, 5, 3]
+
+    }
+    
+    iterator = AnalysisIterator(options, 
+                                AnalysisConfigurator(**example_configuration),
+                                kind='list'
+                                )
+                                
+    bad_n_options = len(options['cv__n_splits'])
+    n_options = len(list(iterator))
+    assert n_options != bad_n_options
+
+    good_n_options = np.prod([len(v) for k, v in options.items()])
+    assert n_options == good_n_options
+    
+
+
+def test_iterator_combined():
+    options = {
+        'estimator': [
+            [('clf1', cluster.MiniBatchKMeans())],
+            [('clf2', cluster.KMeans())],
+            [('clf5', mixture.GaussianMixture())]
+        ],
+        'estimator__clf1__n_clusters'  : range(3, 5),
+        'estimator__clf5__n_components': range(3, 5)
+    }
+
+    example_configuration = {'analysis': Clustering}
+
+    iterator = AnalysisIterator(options, 
+                                AnalysisConfigurator(**example_configuration),
+                                kind='combined'
+                                )
+    
+    n_options = len(list(iterator))
+    assert n_options == 5
+
+    options = {
+        'estimator': [
+            [('clf1', cluster.MiniBatchKMeans())],
+            [('clf2', cluster.KMeans())],
+            [('clf5', mixture.GaussianMixture())]
+        ],
+        'estimator__clf1__n_clusters': range(3, 5),
+        'estimator__clf1__tol': [.1, .3, .5],
+        'estimator__clf5__n_components': range(3, 5),
+        'prepro': [['sample_slicer'], ['feature_slicer']]
+    }
+
+    iterator = AnalysisIterator(options, 
+                                AnalysisConfigurator(**example_configuration),
+                                kind='combined'
+                                )
+    n_options = len(list(iterator))
+    assert n_options == 2*((2*3*1) + (2*1) + 1)
+
+    options = {
+                'estimator': [
+                    [('clf1', cluster.MiniBatchKMeans())],
+                    [('clf1', cluster.KMeans())],
+                    [('clf1', cluster.SpectralClustering())],
+                    [('clf1', cluster.AgglomerativeClustering())],
+                    [('clf5', mixture.GaussianMixture())]
+                ],
+                'estimator__clf1__n_clusters': range(3, 5),
+                'estimator__clf5__n_components': range(2, 4),
+            } 
+
+    iterator = AnalysisIterator(options,
+                                AnalysisConfigurator(**example_configuration),
+                                kind='combined'
+                                )
+
+    n_options = len(list(iterator))
+    assert n_options == (2*4) + 2
+
+    
+
+
 
 def test_iterator_subjectwise():
     pass
